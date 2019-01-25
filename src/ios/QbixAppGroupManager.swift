11c8e44d083873372b5a7f1d546e2ba31eff1007
Igor Martsekha <igor.martsekha@gmail.com>
2019-01-25 20:32:59 +0200

    Added QbixAppGroupManager file

---
+++ src/ios/QbixAppGroupManager.swift
@@ -0,0 +1,56 @@
+//
+//  QbixAppGroupManager.swift
+//  BusinessCardsScan
+//
+//  Created by Igor on 10/29/18.
+//
+
+import Foundation
+
+class QbixAppGroupManager:NSObject {
+    struct keys {
+        static let Name = "Name";
+        static let LastOpen = "LastOpen";
+        static let InstallTime = "InstallTime";
+    }
+    
+    let appGroupId = "group.qbix.apps";
+    let sharedUserDefaults:UserDefaults;
+    
+    let appBundleID:String
+    let installTime:Int64
+    
+    @objc convenience init(appBundleID:String) {
+        self.init(appBundleID: appBundleID, installTime: 0)
+    }
+    
+    @objc init(appBundleID:String, installTime:Int64) {
+        self.sharedUserDefaults = UserDefaults.init(suiteName: appGroupId)!
+        self.appBundleID = appBundleID;
+        self.installTime = installTime;
+    }
+    
+    @objc func initApp() {
+        var appInfo = Dictionary<String, Any>.init();
+        let lastOpen = Date.init().timeIntervalSince1970;
+        let appName = Bundle.init(identifier: "BundleIdentifier")?.object(forInfoDictionaryKey: kCFBundleExecutableKey as String)
+
+        if(self.sharedUserDefaults.dictionary(forKey: self.appBundleID) != nil) {
+            appInfo = self.sharedUserDefaults.dictionary(forKey: self.appBundleID)!;
+        } else {
+            appInfo[QbixAppGroupManager.keys.InstallTime] = Date.init().timeIntervalSince1970
+        }
+        
+        appInfo[QbixAppGroupManager.keys.LastOpen] = lastOpen
+        appInfo[QbixAppGroupManager.keys.Name] = appName
+        if(self.installTime != 0) {
+            appInfo[QbixAppGroupManager.keys.InstallTime] = self.installTime
+        }
+        
+        self.sharedUserDefaults.set(appInfo, forKey: self.appBundleID)
+    }
+    
+    @objc func isAppInstalled(appBundleID:String) -> Bool {
+        return self.sharedUserDefaults.dictionary(forKey: appBundleID) != nil
+    }
+}
